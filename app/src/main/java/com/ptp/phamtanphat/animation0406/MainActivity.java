9aae6783f81a26bc2a987dd30c8d72400854ef91
thuviengithub0208 <khoaphp@yahoo.com>
2018-07-02 19:04:24 +0700

    Scale

--- app/src/main/java/com/ptp/phamtanphat/animation0406/MainActivity.java
+++ app/src/main/java/com/ptp/phamtanphat/animation0406/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.webkit.WebView;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +16,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
     public void AnimationAlpha(View v){
         SetAnimation(R.anim.animation_alpha, v);
@@ -24,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
         //X se tang khi di tu phai sang trai
         //Y se tang khi di tu duoi len tren
         SetAnimation(R.anim.animation_scale, v);
+    }
+    public void AnimationRotate(View v){
+        //X se tang khi di tu phai sang trai
+        //Y se tang khi di tu tren xuong duoi
+        SetAnimation(R.anim.animation_rotate, v);
+    }
+    public void AnimationTranslate(View v){
+        //X se tang khi di tu phai sang trai
+        //Y se tang khi di tu tren xuong duoi
+        SetAnimation(R.anim.animation_translate, v);
     }
     public void SetAnimation(int animation , View v){
         animationView = AnimationUtils.loadAnimation(MainActivity.this,animation);
